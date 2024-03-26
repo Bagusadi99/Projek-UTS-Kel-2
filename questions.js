e48996bd0878506652c7ee712c26eec61f609199
Bagusadi99 <163718900+Bagusadi99@users.noreply.github.com>
2024-03-26 09:39:59 +0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Perusahaan di bidang teknologi informasi yang menciptakan sistem operasi Android adalah?",
+        answer: "B. Google",
+        options: [
+            "A. Microsoft",
+            "B. Google",
+            "C. Intel",
+            "D. Yahoo"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Apa julukan terkenal dari negara Korea Selatan? ",
+        answer: "B. Negeri Gingseng",
+        options: [
+            "A. Negeri Tirai Bambu",
+            "B. Negeri Gingseng",
+            "C. Zamrud Khatulistiwa",
+            "D. Negeri Kincir Angin"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Danau terbesar di dunia adalah?",
+        answer: "C. Danau Kaspia",
+        options: [
+            "A. Danau Toba",
+            "B. Danau Baikal",
+            "C. Danau Kaspia",
+            "D. Danau Victoria"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Tanah yang baik untuk pertanian adalah?",
+        answer: "A. Humus", 
+        options: [
+            "A. Humus",
+            "B. Pasir",
+            "C. Liat",
+            "D. Cadas"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Pada tanggal berapakah Hari Lahir Pancasila diperingati?",
+        answer: "C. 1 Juni",
+        options: [
+            "A. 17 Agustus",
+            "B. 1 Maret",
+            "C. 1 Juni ",
+            "D. 1 Desember "
+        ]
+    }
+];
